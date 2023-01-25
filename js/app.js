f6dae832eec8264ddc2b3c5413c4db942d0136bd
Joy Dey <joydey.bd.2002@gmail.com>
2023-01-25 19:51:05 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,418 @@
+/* ========================
+ ! - Important DOM Elements
+ ==========================*/
+const darkModeToggle = document.querySelector("#chk");
+const modalOpenBtn = document.getElementById("condition-modal-btn");
+const modalContainer = document.querySelector(".quiz-modal-container");
+const modalConditionBox = modalContainer.querySelector(".quiz-modal-condition");
+const exitModal = modalConditionBox.querySelector(".exit-modal");
+const startQuizBtn = modalConditionBox.querySelector(".start-quiz");
+const questionSection = document.querySelector(".question-section");
+const questionText = questionSection.querySelector(".question-text");
+const questionOptionContainer =
+  questionSection.querySelector(".question-options");
+const questionProgressText = questionSection.querySelector(
+  ".question-progress-text"
+);
+const timeText = questionSection.querySelector(".time-count");
+const nextQuiz = questionSection.querySelector(".next-question");
+const scoreElement = questionSection.querySelector(".score");
+const timelineElement = questionSection.querySelector(".timeline");
+const questionProgressbar = questionSection.querySelector(".question-progress");
+const resultSection = document.querySelector(".result-section");
+const canvas = document.getElementById("my-canvas");
+const resultExpression = resultSection.querySelector(".result-expression");
+const resultText = resultSection.querySelector(".result-text");
+const resultFeedback = resultSection.querySelector(".feedback");
+const restart = resultSection.querySelector(".restart");
+const backHome = resultSection.querySelector(".back-to-home");
+
+/* =====================
+ ! - Important Variables - *
+ =======================*/
+let questionIndex = 0;
+let timeCount;
+let userScore = 0;
+let counter; // it will track the timer
+let timelineCounter;
+
+/* ===================
+ ! - Important Elements - *
+ =====================*/
+// tick icon
+const tick = document.createElement("div");
+tick.classList.add("tick-icon");
+tick.innerHTML = `<i class="fa-solid fa-check"></i>`;
+
+// cross icon
+const cross = document.createElement("div");
+cross.classList.add("cross-icon");
+cross.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+
+/* ===================
+ ! - Confetti Settings - *
+ ====================*/
+var confettiSettings = {
+  target: "my-canvas",
+  max: "250",
+  size: "1.8",
+  rotate: true,
+  clock: "25",
+  start_from_edge: true,
+};
+var confetti = new ConfettiGenerator(confettiSettings);
+
+/* ========================
+ ! - Important Functions - *
+ ==========================*/
+/*
+ ! - Dark Mode functionality - *
+ */
+const switchDarkMode = () => {
+  document.body.classList.toggle("dark-mode");
+};
+
+/*
+ ! - Modal Open/Close Fuction - *
+ */
+const modalFunc = (condition) => {
+  if (condition === "open") {
+    modalConditionBox.classList.add("active");
+    if (document.body.classList.contains("dark-mode")) {
+      modalContainer.classList.add("active-dark");
+    } else {
+      modalContainer.classList.add("active");
+    }
+  } else {
+    modalConditionBox.classList.remove("active");
+    if (document.body.classList.contains("dark-mode")) {
+      modalContainer.classList.remove("active-dark");
+    } else {
+      modalContainer.classList.remove("active");
+    }
+  }
+};
+
+/*
+! - Initial show Function
+ * - // initial setup before showing question
+* - // it is used in for later restart and back to home event listener
+ */
+const initialShowQuestion = () => {
+  //Remove the result sections
+  resultSection.style.display = "none";
+  canvas.style.display = "none";
+
+  // the question and score will start from the 0
+  questionIndex = 0;
+  userScore = 0;
+  scoreElement.textContent = userScore;
+};
+
+/*
+ ! - Show Question Function - *
+ */
+const showQuestion = () => {
+  // show Question Section
+  questionSection.style.display = "block";
+
+  // Start the timer
+  timer();
+
+  // Start the timelineFunc
+  timelineFunc();
+
+  // If there is any single option, remove the all option
+  if (document.querySelectorAll(".single-option")) {
+    document
+      .querySelectorAll(".single-option")
+      .forEach((element) => element.remove());
+  }
+
+  // showing question text
+  questionText.textContent = `${questionIndex + 1}. ${
+    quizArr[questionIndex].question
+  }`;
+
+  // Showing question Options
+  const options = quizArr[questionIndex].options;
+
+  for (let option in options) {
+    let singleQuestionElement = document.createElement("div");
+    singleQuestionElement.classList.add("single-option");
+    singleQuestionElement.innerHTML = `
+    <span class="single-option-text"> ${option}.  ${options[option]}</span>`;
+    singleQuestionElement.addEventListener("click", (e) =>
+      selectedAnswer(option, e)
+    );
+    questionOptionContainer.append(singleQuestionElement);
+  }
+
+  // Question Progress Text
+  questionProgress();
+
+  // user can not see the next quiz button initially
+  nextQuiz.style.display = "none";
+};
+
+/*
+ ! - Timeline Fuction - *
+ */
+const timelineFunc = () => {
+  // initially the width will be 100%
+  timelineElement.style.width = `100%`;
+  timeText.textContent = 10;
+
+  timelineCounter = setInterval(() => {
+    // get the time
+    const getTime = Number(timeText.textContent);
+
+    // the width will change, according to the time
+    timelineElement.style.width = `${getTime * 10}%`;
+  }, 1000);
+};
+
+/*
+ ! - After select an option, this selected answer function will be triggered - *
+ */
+const selectedAnswer = (option, e) => {
+  // clear the time
+  clearInterval(counter);
+
+  // clear the timeline
+  clearInterval(timelineCounter);
+
+  // Get the Correct Answer
+  const selectedOption = option;
+  const correctOption = quizArr[questionIndex].answer;
+  //  Checking condition
+  if (selectedOption === correctOption) {
+    userScore += 5;
+    scoreElement.textContent = userScore;
+    showIconTick(e, true);
+  } else {
+    showIconTick(e, false);
+    showCorrectAnswer();
+  }
+
+  // After selected an option, User can not select any of the option again
+  const singleOption = document.querySelectorAll(".single-option");
+  singleOption.forEach((element) => element.classList.add("disabled"));
+
+  // show the next question button
+  nextQuizBtnChange();
+};
+
+/*
+! - Show Icon Tick Function
+*/
+const showIconTick = (e, isTick) => {
+  if (e.target.classList.contains("single-option")) {
+    e.target.children[0].insertAdjacentElement(
+      "afterend",
+      isTick === true ? tick : cross
+    );
+    e.target.classList.add(isTick === true ? "correct" : "incorrect");
+  } else {
+    e.target.insertAdjacentElement("afterend", isTick === true ? tick : cross);
+    e.target.parentNode.classList.add(
+      isTick === true ? "correct" : "incorrect"
+    );
+  }
+};
+
+/*
+! - Show Correct Answer Function
+*/
+const showCorrectAnswer = () => {
+  // find all single options
+  const singleOption = document.getElementsByClassName("single-option");
+
+  // correct answer
+  const correctOption = quizArr[questionIndex].answer;
+
+  // looping the option and show the correct option with tick
+  for (let option of singleOption) {
+    if (option.textContent.trim().slice(0, 1) == correctOption) {
+      option.children[0].insertAdjacentElement("afterend", tick);
+      option.classList.add("correct");
+    }
+
+    // After showing the correct Answer, disable all the option
+    option.classList.add("disabled");
+  }
+};
+
+/*
+! -  Question Progress Function
+*/
+const questionProgress = () => {
+  // Question Progress Loading
+  questionProgressbar.style.width = `${
+    ((questionIndex + 1) / quizArr.length) * 100
+  }%`;
+
+  // Question Progress Text
+  questionProgressText.innerHTML = `<span class="bold">${
+    questionIndex + 1
+  } </span> of  <span class="bold">${quizArr.length}</span> Questions`;
+};
+
+/*
+! - Timer Function
+*/
+const timer = () => {
+  // initially the time start from 10 seconds
+  timeCount = 10;
+  timeText.textContent = timeCount;
+  counter = setInterval(() => {
+    timeCount--;
+    timeText.textContent = timeCount;
+    if (timeCount == 0) {
+      // The score and the timeline width will be 0
+      timeText.textContent = "0" + timeCount;
+      timelineElement.style.width = `0%`;
+
+      // Clear the time counter and timeline counter
+      clearInterval(counter);
+      clearInterval(timelineCounter);
+
+      // show the correct answer
+      showCorrectAnswer();
+
+      // show the next question button
+      nextQuizBtnChange();
+    } else {
+      // Show the time
+      timeText.textContent = "0" + timeCount;
+    }
+  }, 1000);
+};
+
+/*
+! - Next Quiz Button Change function
+*/
+const nextQuizBtnChange = () => {
+  nextQuiz.style.display = "block";
+  if (questionIndex === quizArr.length - 1) {
+    nextQuiz.textContent = "Show Result";
+  } else {
+    nextQuiz.textContent = "Next Question";
+  }
+};
+
+/*
+! - Show Result Function
+*/
+const showResult = () => {
+  questionSection.style.display = "none";
+  resultSection.style.display = "block";
+  canvas.style.display = "block";
+
+  // initially no confetti will shown
+  confett.clear();
+  userScore = 80;
+
+  // Show the result in the result text
+  resultText.textContent = `You scored ${userScore} points`;
+
+  // Giving feedback
+  scoreFeedback(userScore);
+};
+
+/*
+! - Score Feedback Function
+*/
+const scoreFeedback = (userScore) => {
+  // the result will show according to the score
+  if (userScore >= 80 && userScore <= 100) {
+    confettiStart();
+    resultFeedback.textContent = `You are an expert in JavaScript, well done on your outstanding performance. Keep up the good work and continue to showcase your skills.`;
+  } else if (userScore >= 60 && userScore < 80) {
+    confettiStart();
+    resultFeedback.textContent = `You have a solid understanding of JavaScript, keep working to improve your skills and understanding. Your performance is good, but with additional practice and focus, you can excel in this subject. `;
+  } else if (userScore >= 40 && userScore < 60) {
+    confettiStart();
+    resultFeedback.textContent = `You have a fair understanding of JavaScript, but there is room for improvement. It's important to continue practicing and reviewing the material to solidify your understanding and skills. I suggest seeking additional resources and assistance to help you improve in this subject.`;
+  } else {
+    confetti.clear();
+    resultExpression.textContent = `Needs Improvement!!!`;
+    resultFeedback.textContent = `The result of this quiz is not satisfactory, it's clear that you have a limited understanding of JavaScript. It's important to put in extra effort and seek assistance to improve your understanding and skills in this subject. I suggest reviewing the material and practicing regularly in order to improve your performance.`;
+  }
+};
+
+/*
+! - Confetti Start Functionality
+*/
+const confettiStart = () => {
+  setTimeout(() => confetti.render(), 500);
+};
+
+/* ==========================
+! - Event Listeners
+============================*/
+
+// Switch dark mode event listener
+darkModeToggle.addEventListener("click", () => {
+  switchDarkMode();
+});
+
+// Modal Open Button Event listener
+modalOpenBtn.addEventListener("click", () => {
+  modalFunc("open");
+});
+
+// Exit Modal Button Event Listener
+exitModal.addEventListener("click", () => {
+  modalFunc("close");
+});
+
+//When Click on the modal container, the modal will be closed
+modalContainer.addEventListener("click", (e) => {
+  if (e.target == modalContainer) {
+    modalFunc("close");
+  }
+});
+
+// Start Quiz Event Listener
+startQuizBtn.addEventListener("click", () => {
+  // Close the modal
+  modalFunc("close");
+
+  //   Show the question
+  showQuestion();
+
+  // From the beginning the score will be 0
+  scoreElement.textContent = 0;
+});
+
+// Next Quiz Event listener
+nextQuiz.addEventListener("click", () => {
+  // Increasing the question index
+  questionIndex++;
+
+  // if the question completed then the result will show
+  if (questionIndex > quizArr.length - 1) {
+    showResult();
+  } else {
+    // Show the Question
+    showQuestion();
+  }
+});
+
+// Restart Event functionality
+restart.addEventListener("click", () => {
+  // some initial task will show before shoing question
+  // like - removing the result section, and reset the time and score as well
+  initialShowQuestion();
+
+  // show the question
+  showQuestion();
+});
+
+// Back To Home Event listner
+backHome.addEventListener("click", () => {
+  // some initial task will show before shoing question
+  // like - removing the result section, and reset the time and score as well
+  initialShowQuestion();
+});
---
+++ js/quiz.js
@@ -0,0 +1,227 @@
+const quizArr = [
+  {
+    id: 1,
+    question: "What is the correct way to write a JavaScript array?",
+    options: {
+      A: "var colors = 'red', 'green', 'blue';",
+      B: "var colors = ['red', 'green', 'blue']",
+      C: "var colors = (1:'red', 2:'green', 3:'blue');",
+      D: "var colors = 'red' + 'green' + 'blue';",
+    },
+    answer: "B",
+  },
+  // {
+  //   id: 2,
+  //   question: "What is the correct way to write a JavaScript object?",
+  //   options: {
+  //     A: "var person = 'John', 'Doe', 25;",
+  //     B: "var person = ('John', 'Doe', 25);",
+  //     C: "var person = {firstName:'John', lastName:'Doe', age:25};",
+  //     D: "var person = 'firstName:John, lastName:Doe, age:25';",
+  //   },
+  //   answer: "C",
+  // },
+  // {
+  //   id: 3,
+  //   question: "How do you round the number 7.25, to the nearest integer?",
+  //   options: {
+  //     A: "Math.round(7.25)",
+  //     B: "round(7.25)",
+  //     C: "rnd(7.25)",
+  //     D: "math.rnd(7.25)",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 4,
+  //   question: "How do you find the length of a string in JavaScript?",
+  //   options: {
+  //     A: "length(myString)",
+  //     B: "myString.length()",
+  //     C: "myString.length",
+  //     D: "str.length(myString)",
+  //   },
+  //   answer: "C",
+  // },
+  // {
+  //   id: 5,
+  //   question: "How do you create a function in JavaScript?",
+  //   options: {
+  //     A: "function:myFunction()",
+  //     B: "new.function(myFunction)",
+  //     C: "function myFunction()",
+  //     D: "create.function(myFunction)",
+  //   },
+  //   answer: "C",
+  // },
+  // {
+  //   id: 6,
+  //   question:
+  //     "What is the correct way to concatenate two strings in JavaScript?",
+  //   options: {
+  //     A: "myString1 + myString2",
+  //     B: "myString1.concat(myString2)",
+  //     C: "myString1 && myString2",
+  //     D: "myString1.join(myString2)",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 7,
+  //   question: "What is the correct way to define a variable in JavaScript?",
+  //   options: {
+  //     A: "v myVariable = 'hello'",
+  //     B: "variable myVariable = 'hello'",
+  //     C: "var myVariable = 'hello'",
+  //     D: "let myVariable = 'hello'",
+  //   },
+  //   answer: "C",
+  // },
+  // {
+  //   id: 8,
+  //   question: "What is the correct way to create a loop in JavaScript?",
+  //   options: {
+  //     A: "for (i = 0; i < 5; i++)",
+  //     B: "for i = 1 to 5",
+  //     C: "while (i < 5)",
+  //     D: "loop i = 1 to 5",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 9,
+  //   question:
+  //     "What is the correct way to declare a variable using the let keyword?",
+  //   options: {
+  //     A: "let myVariable = 'hello';",
+  //     B: "var myVariable = 'hello';",
+  //     C: "const myVariable = 'hello';",
+  //     D: "new myVariable = 'hello';",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 10,
+  //   question: "What is the difference between let and var?",
+  //   options: {
+  //     A: "let and var are same",
+  //     B: "let has block scope and var has function scope",
+  //     C: "var has block scope and let has function scope",
+  //     D: "let is used to declare constant and var is used to declare variable",
+  //   },
+  //   answer: "B",
+  // },
+  // {
+  //   id: 11,
+  //   question: "What is the correct way to create a closure in JavaScript?",
+  //   options: {
+  //     A: "function myFunction(){var x = 10; return function(){return x}}",
+  //     B: "function myFunction(){let x = 10; return function(){return x}}",
+  //     C: "function myFunction(){const x = 10; return function(){return x}}",
+  //     D: "function myFunction(){x = 10; return function(){return x}}",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 12,
+  //   question: "What is the difference between == and ===?",
+  //   options: {
+  //     A: "== checks for equality and === checks for identity",
+  //     B: "== checks for identity and === checks for equality",
+  //     C: "== and === checks for equality",
+  //     D: "== and === checks for identity",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 13,
+  //   question: "What is the correct way to check if a variable is an array?",
+  //   options: {
+  //     A: "Array.isArray(myArray)",
+  //     B: "myArray instanceof Array",
+  //     C: "myArray.isArray()",
+  //     D: "typeof myArray === 'array'",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 14,
+  //   question: "What is the correct way to declare a class in javascript?",
+  //   options: {
+  //     A: "class MyClass{}",
+  //     B: "function MyClass(){}",
+  //     C: "new MyClass(){}",
+  //     D: "MyClass: function(){}",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 15,
+  //   question: "What is the correct way to check if a variable is undefined?",
+  //   options: {
+  //     A: "typeof myVariable === undefined",
+  //     B: "myVariable === undefined",
+  //     C: "myVariable == undefined",
+  //     D: "myVariable === 'undefined'",
+  //   },
+  //   answer: "B",
+  // },
+  // {
+  //   id: 16,
+  //   question:
+  //     "What is the correct way to convert a string to a number in javascript?",
+  //   options: {
+  //     A: "parseInt(string)",
+  //     B: "parseFloat(string)",
+  //     C: "Number(string)",
+  //     D: "string - 0",
+  //   },
+  //   answer: "C",
+  // },
+  // {
+  //   id: 17,
+  //   question: "What is the correct way to declare a Map in javascript?",
+  //   options: {
+  //     A: "var myMap = new Map();",
+  //     B: "var myMap = {};",
+  //     C: "var myMap = Object.create(null);",
+  //     D: "var myMap = new Object.create(null);",
+  //   },
+  //   answer: "A",
+  // },
+  // {
+  //   id: 18,
+  //   question: "What is the difference between null and undefined?",
+  //   options: {
+  //     A: "null and undefined are same",
+  //     B: "null is an assignment value and undefined is a default value",
+  //     C: "undefined is an assignment value and null is a default value",
+  //     D: "null is an object and undefined is not an object",
+  //   },
+  //   answer: "B",
+  // },
+  // {
+  //   id: 19,
+  //   question:
+  //     "What is the correct way to check if a variable is a number in javascript?",
+  //   options: {
+  //     A: "typeof myVariable === 'number'",
+  //     B: "isNaN(myVariable)",
+  //     C: "myVariable instanceof Number",
+  //     D: "Number.isFinite(myVariable)",
+  //   },
+  //   answer: "D",
+  // },
+  // {
+  //   id: 20,
+  //   question:
+  //     "What is the correct way to check if a variable is an object in javascript?",
+  //   options: {
+  //     A: "typeof myVariable === 'object'",
+  //     B: "myVariable instanceof Object",
+  //     C: "Object.isObject(myVariable)",
+  //     D: "myVariable.constructor === Object",
+  //   },
+  //   answer: "D",
+  // },
+];
